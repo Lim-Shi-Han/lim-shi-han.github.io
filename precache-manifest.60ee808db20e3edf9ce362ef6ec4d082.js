f0327f3f4a2d692e904601eb8c2dcb54eaf99e5b
Lim-Shi-Han <79437795+Lim-Shi-Han@users.noreply.github.com>
2022-01-02 16:19:53 +0800

    Updates

---
+++ precache-manifest.60ee808db20e3edf9ce362ef6ec4d082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "002b778171ad9f5035fb",
+    "url": "/static/css/main.d01ac941.chunk.css"
+  },
+  {
+    "revision": "002b778171ad9f5035fb",
+    "url": "/static/js/main.002b7781.chunk.js"
+  },
+  {
+    "revision": "8c7c8bbdf02d9fa228eb",
+    "url": "/static/js/1.8c7c8bbd.chunk.js"
+  },
+  {
+    "revision": "ae846c063d1efef79c0d",
+    "url": "/static/js/2.ae846c06.chunk.js"
+  },
+  {
+    "revision": "65f92284a6d25a472bbc",
+    "url": "/static/js/runtime~main.65f92284.js"
+  },
+  {
+    "revision": "23ca979ab0aec712a2b8421c71d1c754",
+    "url": "/static/media/mypic.23ca979a.jpg"
+  },
+  {
+    "revision": "ac57690690ae4f8e466310b368847e7e",
+    "url": "/index.html"
+  }
+];
